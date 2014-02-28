2d6a81b6878a8cfa081974a3959d69c76875467a
Jeff Chao <jeffchao@me.com>
2014-02-27 19:46:27 -0800

    WIP on messages facade.

--- models/message/message.go
+++ models/message/message.go
@@ -1,1 +1,15 @@
 package message
+
+import (
+	"github.com/gocql/gocql"
+)
+
+type Schema struct {
+	Id        gocql.UUID `json:"id"`
+	Type      string     `json:"type"`
+	CreatedAt gocql.UUID `json:"created_at"`
+}
+
+func (self Schema) Save() error {
+	return nil
+}
